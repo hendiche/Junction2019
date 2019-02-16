90741787f6bb7dc698fe3339696276ec8008e6a7
Unknown <talles_of_destiny@yahoo.com.sg>
2019-02-16 12:25:19 +0900

    Added axios example usage

--- app/src/screens/HomeScreen.js
+++ app/src/screens/HomeScreen.js
@@ -1,18 +1,33 @@
 import React, {Component} from 'react';
 import { Text, View } from 'react-native';
 
+import axios from 'axios';
+
 export default class HomeScreen extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			name: "asd state",
+			data: {},
 		};
 	}
+
+	componentDidMount() {
+		axios.get('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?number=10&offset=0&query=cheeseburger', {
+			headers: {
+				'X-RapidAPI-Key' : "fce84bdd65msh9340789ab474c49p170d1ajsn3b7244498eac",
+			}
+		})
+		.then((res) => {
+			this.setState({ data: res.data });
+		});
+  }
 
 	render() {
 		return(
 			<View>
-				<Text>{this.state.name}</Text>
+				{this.state.data.results && this.state.data.results.map((item, idx) => (
+						<Text key={idx}>{item.title}</Text>
+				))}
 			</View>
 		);
 	}
